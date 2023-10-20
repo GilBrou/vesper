0fb1b4ecd40455c32be6a4e74f0d6719db1abc04
Gillian Brousse <gillian.brousse@neuf.fr>
2023-10-20 06:32:56 +0200

    working on Json Data

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,19 +3,19 @@
 
 /***COMPONENT***/
 export default function Intro(data) {
-  console.log(data.data1);
+  /*console.log(data.data1);*/
   /*DOM*/
   return (
     <section className="intro">
       <div className="subIntro introLeft ">
         <img src="/img/VesperAstria.webp" alt="image" />
       </div>
-
       <div className="subIntro introRight">
-        {data.data1 ? data.data1.intro.p1 : "chargement..."}
-        {data.data1 ? data.data1.intro.p2 : "chargement..."}
-        {data.data1 ? data.data1.intro.p3 : "chargement..."}
-        {data.data1 ? data.data1.intro.p4 : "chargement..."}
+        <p>{data.data1 ? data.data1.intro.p1 : "chargement..."}</p>
+        <p>{data.data1 ? data.data1.intro.p2 : "chargement..."}</p>
+        <p>{data.data1 ? data.data1.intro.p3 : "chargement..."}</p>
+        <p>{data.data1 ? data.data1.intro.p4 : "chargement..."}</p>
+        <p>{data.data1 ? data.data1.intro.p5 : "chargement..."}</p>
       </div>
     </section>
   );
