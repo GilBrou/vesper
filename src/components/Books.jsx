b77426b247636164a2e8d1bab20d8af0090208eb
Gillian Brousse <gillian.brousse@neuf.fr>
2023-11-12 13:14:50 +0100

    bok works

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -3,27 +3,62 @@
 
 /***COMPONENT***/
 export default function Books(data) {
-  console.log(data.data1.books);
+  /*console.log(data.data1.book);*/
   /*DOM*/
   return (
-    <section className="intro" id="livres">
+    <section className="" id="livres">
       <div className="subIntro2 introLeft2 ">
-        <a href={data.data ? data.data.links.menu0L : "chargement..."}>
+        <a
+          href={data.data1 ? data.data1.book.extrait : "chargement..."}
+          target="Blank_"
+        >
           <div className="bookWrap">
             <div className="book">
+              <p>Lire un extrait</p>
               <img className="cover" src="/img/darkZodiac.webp" alt="image" />
               <div className="spine" />
             </div>
           </div>
         </a>
-
-        <div className="subIntro2 introRight2">
-          <p>{data.data1 ? data.data1.intro.p1 : "chargement..."}</p>
-          <p>{data.data1 ? data.data1.intro.p2 : "chargement..."}</p>
-          <p>{data.data1 ? data.data1.intro.p3 : "chargement..."}</p>
-          <p>{data.data1 ? data.data1.intro.p4 : "chargement..."}</p>
-          <p>{data.data1 ? data.data1.intro.p5 : "chargement..."}</p>
+        <div className="bookButton">
+          <a
+            href={data.data1 ? data.data1.book.buyLink : "chargement..."}
+            target="Blank_"
+          >
+            <i className="fa fa-shopping-cart" aria-hidden="true"></i>Version
+            papier
+          </a>
+          <a
+            href={data.data1 ? data.data1.book.buyLinkEbook : "chargement..."}
+            target="Blank_"
+          >
+            <i className="fa fa-shopping-cart" aria-hidden="true"></i>E-book
+          </a>
         </div>
+        <div className="bookButton bookButton2">
+          <a
+            href={data.data1 ? data.data1.book.avis : "chargement..."}
+            target="Blank_"
+          >
+           Avis de lecteurs
+          </a></div>
+      </div>
+      <div className="subIntro2 introRight2">
+        <div className="rowBook">
+          <p>
+            {data.data1 ? data.data1.book.title : "chargement..."}{" "}
+            {data.data1 ? data.data1.book.subTitle : "chargement..."}
+          </p>
+        </div>
+        <p className="illusT">
+          Illustrations :{" "}
+          {data.data1 ? data.data1.book.Illustrations : "chargement..."}
+        </p>
+        <p>{data.data1 ? data.data1.book.Detail1 : "chargement..."}</p>
+        <p>{data.data1 ? data.data1.book.Detail2 : "chargement..."}</p>
+        <p>{data.data1 ? data.data1.book.Detail3 : "chargement..."}</p>
+        <p>{data.data1 ? data.data1.book.Detail4 : "chargement..."}</p>
+        <p>{data.data1 ? data.data1.book.Detail5 : "chargement..."}</p>
       </div>
     </section>
   );
