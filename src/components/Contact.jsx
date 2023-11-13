9909de95fa94df1afffc434a88589e83ee2f790b
Gillian Brousse <gillian.brousse@neuf.fr>
2023-11-13 23:06:12 +0100

    contact 2

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,22 +9,17 @@ export default function Contact(data) {
   return (
     <section id="contact" className="contact">
       <div className="contactText">
-       <p className="contacTitle">
-          {data.data1 ? data.data1.consult.Ctitle1 : "chargement..."}
-        </p>
-        <p>{data.data1 ? data.data1.consult.Cpara1 : "chargement..."}</p>
-        <p className="contacTitle">
-          {" "}
-          {data.data1 ? data.data1.consult.Ctitle2 : "chargement..."}
-        </p>
         <p>
-          {data.data1 ? data.data1.consult.Cpara2 : "chargement..."}
+          {data.data1 ? data.data1.contact.CpacontactPara : "chargement..."}
+        </p>
+
+        <p className="contacTitle">
           <a
-            href={data.data1 ? data.data1.links.menu6L : "chargement..."}
+            href={
+              data.data1 ? data.data1.contact.contactButton : "chargement..."
+            }
             target="Blank_"
-          >
-            {data.data1 ? data.data1.consult.Cpara3 : "chargement..."}
-          </a>
+          ></a>
         </p>
       </div>
     </section>
